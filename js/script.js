3f73208b8bc470644c7d5f787d16781a344d1ad2
SoQu-PJ <jakobowskipawel02@gmail.com>
2023-08-21 19:39:21 +0200

    add section expand when click button rozwin in section Relizacje
    section expand have modal with masnory(macyjs) and when click photo open biggest modal with photo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 import MacyWrapper from './macyWrapper.js';
 
+// MASONRY IN SECTION REALIZACJE
+
+// masonry
+
 const masonry = new MacyWrapper({
     container: '.masonry',
     mobileFirst: true,
@@ -14,4 +18,48 @@ const masonry = new MacyWrapper({
             }
         }
       }
-});;
+});
+
+// Modal
+document.getElementById('openPopupButton').addEventListener('click', () => {
+    const popupModal = new bootstrap.Modal(document.getElementById('popupModal'));
+    popupModal.show();
+
+    popupModal._element.addEventListener('shown.bs.modal', function () {
+        const masonryModal = new MacyWrapper({
+            container: '.masonry-modal',
+            mobileFirst: true,
+
+            columns: 3,
+            margin:{
+                x:10,
+                y:10
+            },
+
+            breakAt: {
+                760: {
+                    columns: 3,
+                    margin:{
+                        x:30,
+                        y:30
+                    }
+                }
+              }
+        });
+    });
+
+    // Second Modal
+    const masonryModalImg = document.querySelectorAll(".masonry-modal .masonry-img");
+
+    masonryModalImg.forEach(img => {
+        img.addEventListener("click", e => {
+            const imgSrc = img.getAttribute("src");
+
+            const popupModal = new bootstrap.Modal(document.getElementById('secondaryModal'));
+                popupModal.show();
+            document.getElementById("secondary-modal-photo").setAttribute("src", imgSrc);
+        })
+    });
+});
+
+
